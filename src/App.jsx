1548d961682915743f5b4a2f997415bf33df6dfd
Celeste Mariel Soto <celestemariel28@gmail.com>
2026-03-07 21:03:50 -0300

    implementacion de registro de usuario con conexión al backend y verificación de correo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoutesPrincipal from "./routes/RoutesPrincipal";
 import LoginScreen from "./views/LoginScreen";
 import "./assets/css/root.css";
+import RegisterScreen from "./views/RegisterScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
       </Routes>
       <RoutesPrincipal />
     </BrowserRouter>
