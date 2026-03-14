94ad718d914c141fe5367c3624498435fa9d1e43
juanmv32 <juan.m.vargas32@gmail.com>
2026-03-14 15:53:46 -0300

    se agrega la ruta cart

--- src/routes/RoutesPrincipal.jsx
+++ src/routes/RoutesPrincipal.jsx
@@ -5,6 +5,7 @@ import ReservaScreen from "../views/ReservaScreen";
 import TiendaScreen from "../views/TiendaScreen";
 import LoginScreen from "../views/LoginScreen";
 import RegisterScreen from "../views/RegisterScreen";
+import CartScreen from "../views/CartScreen";
 
 const RoutesPrincipal = () => {
   return (
@@ -15,6 +16,7 @@ const RoutesPrincipal = () => {
         <Route path="tienda" element={<TiendaScreen />} />
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
       </Route>
     </Routes>
   );
