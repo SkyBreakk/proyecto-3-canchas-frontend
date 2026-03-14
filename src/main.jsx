a8bf62d494d61352d20372d4e2974106c691a4ca
juanmv32 <juan.m.vargas32@gmail.com>
2026-03-14 15:50:29 -0300

    se importan componentes a main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { UserProvider } from "./context/UserContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import App from "./App.jsx";
 
+import { initMercadoPago } from "@mercadopago/sdk-react";
+
+initMercadoPago(import.meta.env.VITE_MP_PUBLIC_KEY);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </UserProvider>
-    
   </StrictMode>,
 );
